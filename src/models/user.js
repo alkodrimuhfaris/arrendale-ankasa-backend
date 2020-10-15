219e2eb4c1da62301784a7fe7e8b6aa25a1f4e68
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-10-16 05:09:22 +0700

    added update avatar/picture and get detail profile

--- src/models/user.js
+++ src/models/user.js
@@ -3,9 +3,10 @@ const table = 'users'
 
 
 module.exports = {
-    checkUserExist: (data) =>{
+    getDetailProfile: (data) =>{
         return new Promise((resolve, reject) =>{
-            db.query(`SELECT * FROM ${table} WHERE ?`, data, (err, result, _fields)=>{
+            db.query('SELECT * FROM users WHERE ?', data, (err, result, _fields)=>{
+                // console.log(data)
                 if(err) {
                     reject(err);
                 }else {
@@ -14,10 +15,10 @@ module.exports = {
             })
         })
     },
-    signUp: (data={}) => {
+    updateUser: (data={}, uid) => {
         return new Promise((resolve, reject) =>{
             console.log(data)
-            db.query('INSERT INTO users SET ?', data, (err, result, _fields)=> {
+            db.query(`UPDATE ${table} SET ? WHERE id=${uid}`, data, (err, result, _fields)=> {
                 if(err) {
                     reject(err);
                 }else {
