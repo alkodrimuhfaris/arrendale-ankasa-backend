a7de24bd6fc460bfd36bc9d90df1f9c8380056a9
timot24 <timotiussimanjuntak24@gmail.com>
2020-10-16 00:08:12 +0700

    create  model, routes and controller for signup

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -40,5 +40,43 @@ module.exports = {
                 return responseStandard(res, 'Wrong email or password', {}, 400, false)
             }   
         }
+    },
+    signUpController: async(req, res) => {
+        const schema = joi.object({
+            username: joi.string().required(),
+            email: joi.string().required(),
+            password: joi.string().required(),
+            // phone_number: joi.string().required()
+        })
+        
+        let { value: results, error } = schema.validate(req.body)
+        if (error) {
+            return responseStandard(res, 'Error', {error: error.message}, 400, false)
+        } else {
+            const { email } = results
+            const isExist = await authModel.checkUserExist({ email })
+            if (isExist.length > 0) {
+                return responseStandard(res, 'Email already used', {}, 401, false)
+            } else {
+                let { name } = results
+                const salt = await bcrypt.genSalt(10)
+                const hashedPassword = await bcrypt.hash(results.password, salt)
+                results = {
+                    ...results,
+                    password: hashedPassword,
+                }
+                const data = await authModel.signUp(results)
+                if (data.affectedRows) {
+                    results = {
+                        id: data.insertId,
+                        ...results,
+                        password: undefined
+                    }
+                    return responseStandard(res, 'Success to signup', { results }, 200, true)
+                } else {
+                    return responseStandard(res, 'Failed to signup', {}, 401, false)
+                }
+            }
+        }
     }
 }
