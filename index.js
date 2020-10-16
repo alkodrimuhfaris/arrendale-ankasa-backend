07c16760514c3b9ec5089bea5c4fb0a0466021f7
alkodrimuhfaris <63344061+alkodrimuhfaris@users.noreply.github.com>
2020-10-16 14:45:20 +0700

    add explore CRUD

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 require('dotenv').config() //setting environment
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
---
+++ src/helpers/db.js
@@ -0,0 +1,12 @@
+let mysql = require('mysql')
+
+let conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
+
+conn.connect()
+
+module.exports = conn
